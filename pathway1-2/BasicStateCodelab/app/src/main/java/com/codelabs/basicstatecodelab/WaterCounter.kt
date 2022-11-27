6354c1b61cfff4ba37bda2251f66c6944a930934
worlyep <worlyep@gmail.com>
2022-11-28 00:53:34 +0900

    feat: 목록 사용 step 완료

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -41,10 +41,8 @@ fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = M
 
 @Composable
 fun StatefulCounter(modifier: Modifier = Modifier) {
-    var count by remember {
+    var count by rememberSaveable {
         mutableStateOf(0)
     }
-
-    StatelessCounter(count, { count++ })
-    AnotherStatelessMethod(count, { count *= 2 })
+    StatelessCounter(count, { count++ }, modifier)
 }
